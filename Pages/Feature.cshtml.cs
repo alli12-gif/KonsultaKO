42e3863ee6ec40bac5a15eb2db5b2ef4b7026a90
alli12-gif <allionabarcelon733@gmail.com>
2025-09-03 18:33:03 +0800

    Add files via upload

---
+++ Pages/Feature.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KonsultaKO.Pages
+{
+    public class FeatureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Ticket.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KonsultaKO.Pages
+{
+    public class TicketModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
